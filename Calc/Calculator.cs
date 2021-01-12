81893a745f11cbfb44f32570e4443423f2f65148
AndrewGitHabSource <125652a>
2021-01-12 12:41:13 +0200

    start

---
+++ Calc/Calculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Calculator
+    {
+        private List<float> listValues = new List<float>();
+        private List<string> listOperators = new List<string>();
+        public Calculator(List<float> values, List<string> operators)
+        {
+            this.listValues = values;
+            this.listOperators = operators;
+        }
+
+        public string calculate()
+        {
+            string resultString = "0";
+            int index = 0;
+            float result = listValues.ElementAt(index);
+
+            foreach (string operate in listOperators)
+            {
+                switch (operate)
+                {
+                    case "+":
+                        result += listValues.ElementAt(index + 1);
+                        break;
+                    case "-":
+                        result -= listValues.ElementAt(index + 1);
+                        break;
+                    case "*":
+                        result *= listValues.ElementAt(index + 1);
+                        break;
+                    case "/":
+                        result /= listValues.ElementAt(index + 1);
+                        break;
+                }
+
+                index++;
+            }
+
+            resultString = result.ToString();
+
+            return resultString;
+        }
+    }
+}
---
+++ Calc/StringSplitter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class StringSplitter
+    {
+        private string stringCalc;
+        private List<float> listValues = new List<float>();
+        private List<string> listOperators = new List<string>();
+        public string StringCalc
+        {
+            get
+            {
+                return stringCalc;
+            }
+
+            set
+            {
+                stringCalc = value;
+            }
+        }
+
+        public List<float> ListValues
+        {
+            get
+            {
+                return listValues;
+            }
+
+            set
+            {
+                listValues = value;
+            }
+        }
+
+        public List<string> ListOperators
+        {
+            get
+            {
+                return listOperators;
+            }
+
+            set
+            {
+                listOperators = value;
+            }
+        }
+
+        public void execute()
+        {
+            this.findSeparators();
+            this.splitText();
+        }
+
+        private void splitText()
+        {
+            char[] separators = new char[] { '+', '-', '/', '*' };
+            string[] values = this.stringCalc.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var sub in values)
+            {
+                listValues.Add(Int32.Parse(sub));
+            }
+        }
+
+        private void findSeparators()
+        {
+            for(int i = 0; i < this.stringCalc.Length; i++)
+            {
+                if(this.stringCalc[i] == '+' || this.stringCalc[i] == '-' || this.stringCalc[i] == '*' || this.stringCalc[i] == '/')
+                {
+                    listOperators.Add(Char.ToString(this.stringCalc[i]));
+                }
+            }
+        }
+    }
+}
